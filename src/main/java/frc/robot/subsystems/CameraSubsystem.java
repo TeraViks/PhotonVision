d422be924ce9b2f54149ecc5909d6c0ecdb3ab05
SamueljWilson <115675537+SamueljWilson@users.noreply.github.com>
2023-09-26 08:13:48 -0700

    Added more functionality to the CameraSubsystem to get the field relativ pose

--- src/main/java/frc/robot/subsystems/CameraSubsystem.java
+++ src/main/java/frc/robot/subsystems/CameraSubsystem.java
@@ -12,16 +12,23 @@ import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.OperatorConstants.PhotonVisionConstants;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.Constants.PhotonVisionConstants;
 
 public class CameraSubsystem extends SubsystemBase {
   PhotonCamera m_camera;
   PhotonPipelineResult m_result;
   PhotonTrackedTarget m_bestTarget;
-  /** Creates a new PhotonVision. */
+  AprilTagFieldLayout m_aprilTagFieldLayout = 
+    new AprilTagFieldLayout(
+      null, FieldConstants.kFieldLength, FieldConstants.kFieldWidth
+    );
+
   public CameraSubsystem(String CameraName) {
     m_camera = new PhotonCamera(CameraName);
   }
@@ -32,6 +39,14 @@ public class CameraSubsystem extends SubsystemBase {
 
   public List<PhotonTrackedTarget> getAllTargets() {
     return m_result.getTargets();
+  }
+
+  public int getTagID() {
+    return m_bestTarget.getFiducialId();
+  }
+
+  public int getTagID(PhotonTrackedTarget target) {
+    return target.getFiducialId();
   }
 
   public PhotonTrackedTarget getBestTarget() {
@@ -82,11 +97,11 @@ public class CameraSubsystem extends SubsystemBase {
     return target.getDetectedCorners();
   }
 
-  public Transform3d getDistance() {
+  public Transform3d getBestCameraToTarget() {
     return m_bestTarget.getBestCameraToTarget();
   }
 
-  public Transform3d getDistance(PhotonTrackedTarget target) {
+  public Transform3d getBestCameraToTarget(PhotonTrackedTarget target) {
     return target.getBestCameraToTarget();
   }
 
@@ -110,6 +125,22 @@ public class CameraSubsystem extends SubsystemBase {
         Units.degreesToRadians(getPitch(target))
       )
     );
+  }
+
+  public Pose3d getFieldRelativePose() {
+    return PhotonUtils.estimateFieldToRobotAprilTag(
+        getBestCameraToTarget(),
+        m_aprilTagFieldLayout.getTagPose(getTagID()).get(),
+        PhotonVisionConstants.kCameraToRobotOffset
+      );
+  }
+
+  public Pose3d getFieldRelativePose(PhotonTrackedTarget target) {
+    return PhotonUtils.estimateFieldToRobotAprilTag(
+        getBestCameraToTarget(target),
+        m_aprilTagFieldLayout.getTagPose(getTagID(target)).get(),
+        PhotonVisionConstants.kCameraToRobotOffset
+      );
   }
 
   @Override
