1640daa507bde5b17a731bd2c8eb99c9f3df70e6
laptevgaming <81442457+laptevgaming@users.noreply.github.com>
2023-11-02 18:04:31 -0700

    Added a custom AprilTag field

--- src/main/java/frc/robot/subsystems/Camera1Subsystem.java
+++ src/main/java/frc/robot/subsystems/Camera1Subsystem.java
@@ -7,28 +7,40 @@ package frc.robot.subsystems;
 import java.util.List;
 import java.util.Optional;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.common.hardware.VisionLEDMode;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
-import org.photonvision.targeting.TargetCorner;
+import org.photonvision.PhotonPoseEstimator;
 
+import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.FieldConstants;
 import frc.robot.Constants.PhotonVisionConstants;
 
 public class Camera1Subsystem extends SubsystemBase {
+  // private final double kAprilTagOffsetMeters = 0.0762;
+  private final double kAprilTagOffsetMeters = 0.0;
+
   PhotonCamera m_camera;
   PhotonPipelineResult m_result;
   Optional<PhotonTrackedTarget> m_lowestAmbiguityTarget;
-  //TODO: Fill in the null for the apriltag
+  AprilTag aprilTag1 = new AprilTag(1, new Pose3d(new Translation3d(4.0, 0.5+kAprilTagOffsetMeters, PhotonVisionConstants.kTarget1HeightMeters), new Rotation3d(0.0, 0.0, Math.PI)));
+  AprilTag aprilTag2 = new AprilTag(2, new Pose3d(new Translation3d(4.0, 2.5+kAprilTagOffsetMeters, PhotonVisionConstants.kTarget2HeightMeters), new Rotation3d(0.0, 0.0, Math.PI)));
+  AprilTag aprilTag3 = new AprilTag(3, new Pose3d(new Translation3d(4.0, 1.5+kAprilTagOffsetMeters, PhotonVisionConstants.kTarget3HeightMeters), new Rotation3d(0.0, 0.0, Math.PI)));
+  List<AprilTag> aprilTagList = Arrays.asList(aprilTag1, aprilTag2, aprilTag3);
+
   AprilTagFieldLayout m_aprilTagFieldLayout = 
     new AprilTagFieldLayout(
-      null, FieldConstants.kFieldLength, FieldConstants.kFieldWidth
+      aprilTagList, FieldConstants.kFieldLength, FieldConstants.kFieldWidth
     );
 
   public Camera1Subsystem(String CameraName) {
