f7d947a7559f3242ae95fba286238d5ee0cafb10
SamueljWilson <samspamgbot@gmail.com>
2024-01-13 17:03:43 -0800

    Updated the testing command for 2 cameras

--- src/main/java/frc/robot/commands/CameraTest.java
+++ src/main/java/frc/robot/commands/CameraTest.java
@@ -15,13 +15,13 @@ import edu.wpi.first.math.kinematics.DifferentialDriveKinematics;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.Camera1Subsystem;
+import frc.robot.subsystems.CameraSubsystem;
 
 public class CameraTest extends Command {
-  private final Camera1Subsystem m_camera;
+  private final CameraSubsystem m_photonCameras;
   private final DifferentialDrivePoseEstimator m_poseEstimator;
   private Timer m_timer;
-  public CameraTest(Camera1Subsystem camera) {
+  public CameraTest(CameraSubsystem cameras) {
     m_timer = new Timer();
     m_poseEstimator =
       new DifferentialDrivePoseEstimator(
@@ -32,8 +32,8 @@ public class CameraTest extends Command {
           new Pose2d(),
           VecBuilder.fill(0.005, 0.005, Math.toRadians(1)),
           VecBuilder.fill(0.05, 0.05, Math.toRadians(5)));
-    m_camera = camera;
-    addRequirements(camera);
+    m_photonCameras = cameras;
+    addRequirements(cameras);
   }
 
   // Called when the command is initially scheduled.
@@ -45,11 +45,20 @@ public class CameraTest extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Optional<EstimatedRobotPose> robotPoseEstimator = m_camera.getFieldRelativePoseEstimator();
-    if (robotPoseEstimator.isEmpty()) return;
-    SmartDashboard.putString("Estimator Pose", robotPoseEstimator.get().estimatedPose.getTranslation().toString() + " | Rotation: " + Math.toDegrees(robotPoseEstimator.get().estimatedPose.getRotation().getAngle()*Math.PI));
+    // Optional<EstimatedRobotPose> robotPoseEstimatorCam2 = m_photonCameras.getFieldRelativePoseEstimatorCam2();
+    // if (!robotPoseEstimatorCam2.isEmpty()) {
+    //   m_poseEstimator.addVisionMeasurement(robotPoseEstimatorCam2.get().estimatedPose.toPose2d(), robotPoseEstimatorCam2.get().timestampSeconds);
+    //   SmartDashboard.putString("Estimator Pose 2", robotPoseEstimatorCam2.get().estimatedPose.getTranslation().toString() + " | Rotation: " + Math.toDegrees(robotPoseEstimatorCam2.get().estimatedPose.getRotation().getAngle()*Math.PI));
+    // }
+
+    Optional<EstimatedRobotPose> robotPoseEstimatorCam1 = m_photonCameras.getFieldRelativePoseEstimatorCam1();
+    if (!robotPoseEstimatorCam1.isEmpty()) {
+      m_poseEstimator.addVisionMeasurement(robotPoseEstimatorCam1.get().estimatedPose.toPose2d(), robotPoseEstimatorCam1.get().timestampSeconds);
+      SmartDashboard.putString("Estimator Pose 1", robotPoseEstimatorCam1.get().estimatedPose.getTranslation().toString() + " | Rotation: " + Math.toDegrees(robotPoseEstimatorCam1.get().estimatedPose.getRotation().getAngle()*Math.PI));
+    }
+
     m_poseEstimator.update(new Rotation2d(), 0, 0);
-    m_poseEstimator.addVisionMeasurement(robotPoseEstimator.get().estimatedPose.toPose2d(), robotPoseEstimator.get().timestampSeconds);
+
     SmartDashboard.putString("DifferentialDriveEstimatorPose", m_poseEstimator.getEstimatedPosition().toString());
   }
 
